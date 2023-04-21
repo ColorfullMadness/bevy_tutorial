236dac4735c0dbf3b38b4080e639a4015bfe7ed3
ColorfullMadness <podniebnyvader@gmail.com>
2023-04-22 01:28:42 +0200

    Added main menu.

--- src/main_menu/mod.rs
+++ src/main_menu/mod.rs
@@ -1,14 +1,29 @@
+mod components;
+mod styles;
+mod systems;
+
 use bevy::prelude::*;
+
+use crate::AppState;
+
+use self::systems::{layout::{spawn_main_menu, despawn_main_menu}, interactions::{interact_with_play_button, interact_with_quit_button}};
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(main_menu);
+        app
+        //OnEnter State Systems
+        .add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+        //Systems
+        .add_systems(
+            (
+                interact_with_play_button,
+                interact_with_quit_button
+            ).in_set(OnUpdate(AppState::MainMenu))
+        )
+        //OnExit State Systems
+        .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
     }    
-}
-
-pub fn main_menu(){
-    println!("You are in main menu!");
 }
 
